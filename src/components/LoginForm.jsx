3a9766498cfca7a70d0849c5784e328b930e5bce
nsamedov <nsamedov1985@gmail.com>
2024-03-14 00:13:26 +0200

    wip

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/loginForm.css";
+import users from "../data/users";
 
-export default (prop) => {
+export default () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="container">
       <span>Login</span>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button onClick={() => localStorage.setItem("username", username)}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          const user = users.find((u) => u.username === username);
+          if (user === undefined) {
+            alert("user not found");
+            return;
+          }
+          if (user.password !== password) {
+            alert("wrong password");
+            return;
+          }
+          localStorage.setItem("username",user.username)
+          location.pathname="/"
+        }}
+      >
         Submit
       </button>
       <div>
