6eba0fe65f7c28696e72c90c0bf90b2e1b5b6cc5
zehavit1994 <zehavit.kras@gmail.com>
2024-03-13 23:03:26 +0200

    users

---
+++ src/data/users.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    username: "brad",
+    email: "brad@pit.il",
+    password: 123456,
+  },
+  {
+    username: "jane",
+    email: "angelina@jolie.il",
+    password: 123123,
+  },
+  {
+    username: "john",
+    email: "angelina@jolie.il",
+    password: 123123,
+  },
+];
